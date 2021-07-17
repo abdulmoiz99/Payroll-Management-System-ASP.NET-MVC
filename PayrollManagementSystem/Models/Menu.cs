55928a7477dd80654a6ba702d4e4e88d86b54e40
Abdul Moiz <moiza0799@gmail.com>
2021-07-17 14:27:52 +0500

    Dashboard Page Added

---
+++ PayrollManagementSystem/Models/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PayrollManagementSystem.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+
+        public int MainID { get; set; }
+
+        public string Type { get; set; }
+
+        public string Title { get; set; }
+
+        public string Name { get; set; }
+    }
+}
